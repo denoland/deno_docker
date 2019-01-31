cfb75d037a6b3bc64487523120fe532cb3c50ffe
Andy Hayden <andyhayden1@gmail.com>
2019-01-31 09:37:13 -0800

    :ice_cream: bump version to 0.2.8
    Also add a deploy script to make it trivial to push to dockerhub.

---
+++ _dockerhub-deploy.ts
@@ -0,0 +1,49 @@
+import {run} from "deno";
+
+
+const DENO_VERSION = '0.2.8';
+
+
+async function main(){
+  console.log("Building and pushing", DENO_VERSION);
+  for (let name of ["alpine", "debian", "ubuntu"]) {
+    const p1 = run({
+      args: ["docker", "build", "-f", `${name}.dockerfile`, "-t", `hayd/deno:${name}-${DENO_VERSION}`, "."],
+      stdout: "piped",
+    });
+    const s1 = await p1.status();
+    if (!s1.success) {
+      const o1 = new TextDecoder().decode(await p1.output());
+      console.log(o1);
+      console.error(`Building ${name} failed`);
+      return;
+    }
+ 
+    const p2 = run({
+        args: ["docker", "run", "-it", name],
+        stdout: "piped",
+    });
+    const s2 = await p2.status();
+    const o2 = new TextDecoder().decode(await p2.output());
+    console.log(name, o2);
+    if (!s2.success) {
+      console.error("Failed to run", name);
+      return;
+    }
+
+    const p3 = run({
+      args: ["docker", "push", `hayd/deno:${name}-${DENO_VERSION}`],
+      stdout: "piped",
+    })
+    const s3 = await p3.status();
+    if (!s3.success) {
+      const o3 = new TextDecoder().decode(await p3.output());
+      console.log(o3);
+      console.error(`Pushing ${name} failed`);
+      return;
+    }
+  }
+}
+
+main();
+
--- debian.dockerfile
+++ debian.dockerfile
@@ -1,6 +1,6 @@
 FROM debian:stretch-slim
 
-ENV DENO_VERSION=0.2.6
+ENV DENO_VERSION=0.2.8
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get -qq update && \
