db294b766b813194b6de26f28d8dd570058b9270
Andy Hayden <andyhayden1@gmail.com>
2019-11-03 15:07:03 -0800

    Propogate init and invocation errors in lambda

--- aws-lambda/lambda.ts
+++ aws-lambda/lambda.ts
@@ -1,7 +1,6 @@
 export async function handler(event) {
-  const body = { hello: "deno" };
   return {
     statusCode: 200,
-    body: JSON.stringify(body)
+    body: `Welcome to deno ${Deno.version.deno} 🦕`
   };
 }
