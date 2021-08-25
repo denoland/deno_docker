aeb390578887cd8008d1b464fb270cc7ad282034
Felipe Santos <felipecassiors@gmail.com>
2021-08-25 10:50:35 -0300

    Check for options in the entrypoint (#158)

--- _entry.sh
+++ _entry.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ "$1" != "${1#-}" ]; then
+    # if the first argument is an option like `--help` or `-h`
+    exec deno "$@"
+fi
+
 case "$1" in
     bundle | cache | compile | completions | coverage | doc | eval | fmt | help | info | install | lint | lsp | repl | run | test | types | upgrade )
+    # if the first argument is a known deno command
     exec deno "$@";;
 esac
 
