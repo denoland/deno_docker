43ab9f5fd4c1cbecd3a2068e5deca20b89e7e19b
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-10-01 03:02:15 +0100

    2.0.0-rc.8 (#418)

--- bin.dockerfile
+++ bin.dockerfile
@@ -1,4 +1,4 @@
-ARG DENO_VERSION=2.0.0-rc.7
+ARG DENO_VERSION=2.0.0-rc.8
 
 
 FROM buildpack-deps:20.04-curl AS download
--- tools/release/create_pr.ts
+++ tools/release/create_pr.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-net --allow-run=git --no-check
+#!/usr/bin/env -S deno run -R -W -E -N -S --allow-run=git --no-check
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { createOctoKit, getGitHubRepository, parseYaml } from "./deps.ts";
 import { loadRepo } from "./repo.ts";
